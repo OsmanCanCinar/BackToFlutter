7417439f1b3dadd7396a0bed0df156d12f0f785d
OsmanCanCinar <osmancancinar@gmail.com>
2024-05-28 15:22:48 +0300

    stack

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
- const MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +31,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   List<Color> colors = [
     Colors.red,
     Colors.green,
@@ -47,72 +46,116 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Flutter Demo'),
       ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Container(
-            width: 300,
-            height: 150,
-            color: colors[0],
-            margin: const EdgeInsets.only(bottom: 15.9,left: 15.9,right: 5.9),
+      body: Stack(fit: StackFit.loose, children: [
+        Positioned(
+          bottom: 100,
+          left: 100,
+          child: Container(
+            color: Colors.red,
+            child: const Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text('a'),
+                Text('a'),
+                Text('a'),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.blue,
+          width: 150,
+          height: 650,
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text('a'),
+              Text('a'),
+              Text('a'),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class LayoutWidget extends StatelessWidget {
+  const LayoutWidget({
+    super.key,
+    required this.colors,
+  });
+
+  final List<Color> colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      // mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          width: 300,
+          height: 150,
+          color: colors[0],
+          margin: const EdgeInsets.only(bottom: 15.9, left: 15.9, right: 5.9),
+          child: Text(
+            'Lorem ipsum dolor sit amet',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+        const Spacer(),
+        Container(
+          width: 300,
+          height: 150,
+          color: colors[1],
+          padding: const EdgeInsets.symmetric(horizontal: 15.9, vertical: 5.9),
+          child: Flexible(
             child: Text(
               'Lorem ipsum dolor sit amet',
               style: Theme.of(context).textTheme.headlineMedium,
-
+              overflow: TextOverflow.fade,
+              textDirection: TextDirection.rtl,
             ),
           ),
-          const Spacer(),
-          Container(
-            width: 300,
-            height: 150,
-            color: colors[1],
-            padding: const EdgeInsets.symmetric(horizontal: 15.9,vertical: 5.9),
-            child: Flexible(
-              child: Text(
-                'Lorem ipsum dolor sit amet',
-                style: Theme.of(context).textTheme.headlineMedium,
-                overflow: TextOverflow.fade,
-              
-                textDirection: TextDirection.rtl,
-              ),
+        ),
+        Container(
+          width: 300,
+          height: 150,
+          color: colors[2],
+          padding: const EdgeInsets.fromLTRB(15.9, 5.9, 15.9, 5.9),
+          child: Text(
+            maxLines: 2,
+            'Lorem ipsum dolor sit amet asdasdasdasdasda',
+            style: Theme.of(context).textTheme.headlineMedium,
+            overflow: TextOverflow.ellipsis,
+            textWidthBasis: TextWidthBasis.longestLine,
+            textAlign: TextAlign.center,
+            textHeightBehavior: const TextHeightBehavior(
+              applyHeightToFirstAscent: true,
+              applyHeightToLastDescent: false,
+              leadingDistribution: TextLeadingDistribution.even,
             ),
-          ),Container(
+          ),
+        ),
+        Expanded(
+          child: Container(
             width: 300,
             height: 150,
-            color: colors[2],
-            padding: const EdgeInsets.fromLTRB(15.9,5.9,15.9,5.9),
+            color: colors[3],
+            padding: const EdgeInsets.all(15.9),
             child: Text(
-              maxLines: 2,
-              'Lorem ipsum dolor sit amet asdasdasdasdasda',
+              'Lorem ipsum dolor sit amet',
               style: Theme.of(context).textTheme.headlineMedium,
+              maxLines: 1,
               overflow: TextOverflow.ellipsis,
-              textWidthBasis: TextWidthBasis.longestLine,
-              textAlign: TextAlign.center,
-              textHeightBehavior: const TextHeightBehavior(
-                applyHeightToFirstAscent: true,
-                applyHeightToLastDescent: false,
-                leadingDistribution: TextLeadingDistribution.even,
-              ),
-            ),
-          ),Expanded(
-            child: Container(
-              width: 300,
-              height: 150,
-              color: colors[3],
-              padding: const EdgeInsets.all(15.9),
-              child: Text(
-                'Lorem ipsum dolor sit amet',
-                style: Theme.of(context).textTheme.headlineMedium,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                textDirection: TextDirection.ltr,
-              ),
+              textDirection: TextDirection.ltr,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
